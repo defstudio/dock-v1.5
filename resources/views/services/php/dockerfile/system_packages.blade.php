8669a75d9fc302c83a1c715c06e899f6c4b81f63
Fabio Ivona <fabio.ivona@defstudio.it>
2022-12-13 13:02:17 +0100

    switch mariadb to mysql in php tools

--- resources/views/services/php/dockerfile/system_packages.blade.php
+++ resources/views/services/php/dockerfile/system_packages.blade.php
@@ -1,7 +1,17 @@
 {{--@formatter:off--}}
 <?php /** @var \App\Docker\Services\Php $service */ ?>
 # Packages installation
+
 RUN apt-get update && \
 @foreach($service->systemPackages() as $package)
     apt-get install -y --no-install-recommends {!! $package !!}{!! $loop->last ? '' : " && \\" !!}
 @endforeach
+
+@if($service->isMySqlClientEnabled())
+    RUN apt-get update && apt-get install -y --no-install-recommends wget lsb-release && \
+    wget -O mysql-apt-config.deb  https://dev.mysql.com/get/mysql-apt-config_0.8.24-1_all.deb && \
+    DEBIAN_FRONTEND=noninteractive dpkg -i mysql-apt-config.deb && \
+    apt-get update && \
+    apt-get install -y --no-install-recommends mysql-client && \
+    rm mysql-apt-config.deb; \
+@endif
