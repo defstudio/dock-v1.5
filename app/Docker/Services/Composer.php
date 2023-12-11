c441ea3d6b0600c62d0e2e09309b99ff2232fd8f
Fabio Ivona <fabio.ivona@defstudio.it>
2023-12-11 12:57:05 +0100

    persist composer auth.json

--- app/Docker/Services/Composer.php
+++ app/Docker/Services/Composer.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Str;
 class Composer extends Php
 {
     public const ASSET_START_SCRIPT_PATH = 'build/composer/start_script.sh';
+    public const ASSET_AUTH_JSON_PATH = 'auth.json';
 
     protected function configure(): void
     {
@@ -20,6 +21,11 @@ class Composer extends Php
 
         $this->serviceDefinition->set('restart', 'no');
         $this->serviceDefinition->unset('depends_on');
+
+        $this->addVolume(
+            self::HOST_SERVICES_PATH."/$this->name/".self::ASSET_AUTH_JSON_PATH,
+            '/.composer/auth.json',
+        );
     }
 
     public function commands(): array
@@ -33,6 +39,7 @@ class Composer extends Php
     {
         $this->publishStartScript();
         $this->appendDockerfile();
+        $this->ensureAuthJsonExists();
     }
 
     private function publishStartScript(): void
@@ -50,5 +57,14 @@ class Composer extends Php
         }
 
         $this->assets()->append(self::ASSET_DOCKERFILE_PATH, $dockerfile);
+    }
+
+    private function ensureAuthJsonExists(): void
+    {
+        if ($this->assets()->exists(self::ASSET_AUTH_JSON_PATH)) {
+            return;
+        }
+
+        $this->assets()->put(self::ASSET_AUTH_JSON_PATH, '{}');
     }
 }
